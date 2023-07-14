bce0493a0ffd8ac9778302aa42c728fa4f214a84
joaomateus181 <joaomateusapg01@gmail.com>
2023-07-14 14:12:00 -0300

    criado salecard

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Navbar from '@/components/navbar/navbar'
 import Subtitle from '@/components/tipography/subtitle/subtitle'
 import Container from '@/components/container/container'
+import SaleCard from '@/components/cards/SaleCard/saleCard'
 
 export default function Home() {
   return (
@@ -17,6 +18,9 @@ export default function Home() {
         <Container>
           <div>
             <Subtitle>Promoções</Subtitle>
+          </div>
+          <div>
+            <SaleCard />
           </div>
           <div>
             <Subtitle>Outros Jogos</Subtitle>
